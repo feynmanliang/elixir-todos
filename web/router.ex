131d89971b4f07fd6487e83a25630de562b3e6ce
Feynman Liang <feynman.liang@gmail.com>
2017-12-28 12:01:41 -0800

    Adds page for elm rendering

--- web/router.ex
+++ web/router.ex
@@ -2,12 +2,25 @@ defmodule Todos.Router do
   use Todos.Web, :router
   alias Todos.Guardian
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   pipeline :authenticated do
     plug :ensure_jwt
+  end
+
+  scope "/", Todos do
+    pipe_through :browser
+    get "/", PageController, :index
   end
 
   scope "/api/auth", Todos do
