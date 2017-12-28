82de1736d5c10f81bfe5ea86550ad4e45fdacb59
Feynman Liang <feynman.liang@gmail.com>
2017-12-27 16:43:41 -0800

    Adds JWT validation

--- web/router.ex
+++ web/router.ex
@@ -1,30 +1,39 @@
 defmodule Todos.Router do
   use Todos.Web, :router
-
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
+  alias Todos.Guardian
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  pipeline :authenticated do
+    plug :ensure_jwt
+  end
+
+  scope "/api/auth", Todos do
+    pipe_through [:api]
+    post "/login", UserController, :login
+    put "/logout", UserController, :logout
+  end
 
   scope "/api", Todos do
-    pipe_through :api
+    pipe_through [:api, :authenticated]
 
     resources "/todos", TodoController, except: [:new, :edit]
     resources "/users", UserController, except: [:new, :edit]
-    post "/users/login", UserController, :login
-    put "/users/logout", UserController, :logout
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Todos do
-  #   pipe_through :api
-  # end
+  defp ensure_jwt(conn, _params) do
+    with {:ok, user, _claims} <- conn.req_headers
+      |> Enum.find(fn(element) -> match?({"bearer", _}, element) end)
+      |> elem(1)
+      |> Guardian.resource_from_token() do
+      assign(conn, :user, user)
+    else
+      _ ->
+        conn
+        |> send_resp(401, "Invalid credentials")
+        |> halt()
+    end
+  end
 end
